0f536c459659b23198aea59d45d156f976c6f20a
NehaKoppikar <neha.koppikar@gmail.com>
2026-05-10 19:03:34 +0000

    Deploying to gh-pages from @ NehaKoppikar/nehakoppikar.github.io@a680bc94093c8bfc1b5eb1b58308eb8082eb5b28 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "post-omscs-course-review-cs-6750-human-computer-interaction",
         
           title: "OMSCS Course Review: CS 6750 Human-Computer Interaction",
